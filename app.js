9b525311c3631865c8110ca448220d798ace847e
mac <mac@macs-iMac.local>
2018-05-20 17:46:16 -0700

    started over

--- app.js
+++ app.js
@@ -1,43 +1,35 @@
-// an array to store the class names of the cards 
-let openCards = [];
-// an array to manipulate the cards on the HTML after action
-let currentElements = [];
 
-// Create a list that holds all of your cards
-let cardConfig = [
-'fa fa-diamond',
-'fa fa-diamond',
-'fa fa-paper-plane-o',
-'fa fa-paper-plane-o',
-'fa fa-anchor',
-'fa fa-anchor',
-'fa fa-bolt',
-'fa fa-bolt',
-'fa fa-cube',
-'fa fa-cube',
-'fa fa-leaf',
-'fa fa-leaf'];
+/*
+ * Create a list that holds all of your cards
+ */
+const cardArray = [
+	'<i class = "fa fa-diamond"></i>',
+	'<i class = "fa fa-diamond"></i>',
+	'<i class = "fa fa-paper-plane-o"></i>',
+	'<i class = "fa fa-paper-plane-o"></i>',
+	'<i class = "fa fa-anchor"> </i>',
+	'<i class = "fa fa-anchor"></i>',
+	'<i class = "fa fa-bolt"></i>',
+	'<i class = "fa fa-bolt"></i>',
+	'<i class = "fa fa-cube"></i>',
+	'<i class = "fa fa-cube"></i>',
+	'<i class = "fa fa-leaf"></i>',
+	'<i class = "fa fa-leaf"></i>',
+	'<i class = "fa fa-bicycle"></i>',
+	'<i class = "fa fa-bicycle"></i>',
+	'<i class = "fa fa-bomb"></i>',
+	'<i class = "fa fa-bomb"></i>',
+]
 
-// - shuffle the list of cards using the provided "shuffle" method below
-let newCardConfig = shuffle(cardConfig);
-// create elements and document fragment for performance
-const deckClass = document.querySelector('.deck');
-const fragment = document.createDocumentFragment();
-
-// Display the cards on the page
-// iterate through shuffled card list and add them to the document fragment
-newCardConfig.forEach(function(card) {
-	let iElement = document.createElement('i');
-	iElement.setAttribute('class', card);
-	const liElement = document.createElement('li');
-	liElement.setAttribute('class', 'card');
-	liElement.appendChild(iElement);
-	deckClass.appendChild(liElement);
-	// append elements in loop to a fragment doc for performace
-	fragment.appendChild(deckClass);
-});
-// append ALL li elements after the loop
-document.body.appendChild(fragment);
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+function display() {
+	shuffledCardArray = shuffle(cardArray);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -54,52 +46,14 @@ function shuffle(array) {
     return array;
 }
 
-function moveCounter() {
 
-}
-
-function matchCheck() {
-	// if the list already has another card, check to see if the two cards match
-	if (openCards.length === 2) {
-	 	if(openCards[0] === openCards[1]) {
-	 		for (let i = 0; i <= currentElements.length; i++) {
-	 			let currentIndex = currentElements[i];
-	 			currentIndex.className += ' match';
-	 		}
-	 		// empty array when done checking two cards
-	 		currentElements = [];
-	 	} else {
-	 		for (let i = 0; i <= currentElements.length; i++) {
-	 			let currentIndex = currentElements[i];
-	 			currentIndex.className += ' mismatch';
-	 		}
-	 		currentElements = [];
-	 		console.log(currentElements.length);
-	 	}
-	} 
-}
-
-// * set up the event listener for a card. If a card is clicked:
-let deckClassElements = document.getElementsByClassName('card');
-Array.from(deckClassElements).forEach(function(element) {
-	element.addEventListener('click', function whenClicked() {
-		// displays number of moves player has made
-		moveCounter();
-		// display the card's symbol 
-		element.classList.add('open');
-		element.classList.add('show');
-		// add the card to a *list* of "open" cards
-		openCards.push(element.querySelector('i').className);
-		currentElements.push(element);
-		matchCheck();
-			
-	});
-
-});
- // 	
- // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- // *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- 
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
